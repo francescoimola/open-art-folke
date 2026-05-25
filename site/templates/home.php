49f890dffabf26350c6a89f3344af37e12d5a557
Francesco Imola <33965523+francescoimola@users.noreply.github.com>
2026-05-25 13:03:55 +0100

    extend graffiti ui + remove all p5js code

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,31 +1,24 @@
 <?php snippet('header') ?>
 
-<main>
+
   <section class="hero">
-    <h2><?= $page->subtitle() ?></h2>
-    <h1><?= $page->title() ?></h1>
-    <div class="text">
-      <?= $page->text()->kt() ?>
+    <div class="hero-title">
+      <h1><?= $page->title() ?></h1>
     </div>
   </section>
-
-  <section class="section-one section">
-    <div class="layout-readable stack">
-      <h2>About the project</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </div>
+  
+<main class="readable stack">
+  <section class="stack" style="margin-top: 1rem;">
+    <h2>About the project</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   </section>
 
-  <section class="section-two section">
-    <div class="layout-readable stack">
-      <h2>Get involved</h2>
-      <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper.</p>
-      <p>Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.</p>
-    </div>
+  <section class="stack">
+    <h2>Get involved</h2>
+    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper.</p>
+    <p>Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.</p>
   </section>
 </main>
 
-<script src="https://cdn.jsdelivr.net/npm/p5/lib/p5.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/p5.brush/dist/p5.brush.js"></script>
 <?php snippet('footer') ?>
--- site/templates/test.php
+++ site/templates/test.php
@@ -1,17 +1,19 @@
 <?php snippet('header') ?>
 
 <main>
-  <section class="hero block-padding inline-padding">
-    <h1><?= $page->title() ?></h1>
+  <section class="hero">
+    <div class="hero-title">
+      <h1><?= $page->title() ?></h1>
+    </div>
   </section>
 
-  <section class="section-one block-padding inline-padding">
+  <section class="section-one">
     <h2>About the project</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
     <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   </section>
 
-  <section class="section-two block-padding inline-padding">
+  <section class="section-two">
     <h2>Get involved</h2>
     <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper.</p>
     <p>Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.</p>
