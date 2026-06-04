fb76a438ee5a0eab80d2aca166a19342410d0a45
Francesco Imola <33965523+francescoimola@users.noreply.github.com>
2026-06-04 15:01:33 +0100

    Create about.php

---
+++ site/templates/about.php
@@ -0,0 +1,55 @@
+<?php snippet('header') ?>
+
+<section class="page-hero panel even theme-brand stack-section">
+  <h1>About Open Art</h1>
+</section>
+
+<section class="panel even theme-paper stack-section flowing stack gap-xxl">
+  <div class="tagline stack gap-l">
+    <p class="statement">Under the umbrella of Open Art Folke, creatives come together all year round for labs, events, and conversations.</p>
+    <p class="statement">Every so often, we invite the whole town to join us for a mighty festival, where you can meet the makers in their studios, their homes or perhaps in more unlikely corners of Folkestone.</p>
+  </div>
+
+  <figure class="layout-split">
+      <img src="/assets/images/artist-in-studio.jpg" alt="Artist seated among prints and sculptures in their Folkestone studio">
+  </figure>
+</section>
+
+<section class="panel even theme-paper stack gap-xl stack-section flowing">
+  <div class="stack gap-xl">
+    <h2>The people behind Open Art</h2>
+    <p class="fs-m readable">OAF is a nonprofit, volunteer-run collective led by artists for artists. We don't guarantee running a festival each year, but when we do run, our incredible team is what makes it possible.</p>
+  </div>
+
+  <div class="table table-flush">
+    <table class="fs-m">
+      <thead class="visually-hidden">
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Area</th>
+          <th scope="col">Who they are</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr><td>Thurle Wright</td><td>Co-Chair</td><td>Artist</td></tr>
+        <tr><td>Becca Pelly-Fry</td><td>Co-Chair</td><td>Curator &amp; Cultural Consultant</td></tr>
+        <tr><td>Steven Aron Williams</td><td>Marketing &amp; Comms</td><td>Artist</td></tr>
+        <tr><td>Simon Rickard</td><td>Programmes &amp; Operations</td><td>Artist Technician</td></tr>
+        <tr><td>Francesco Imola</td><td>Marketing &amp; Comms</td><td>Designer &amp; Artist</td></tr>
+        <tr><td>Sangmin Cha</td><td>Programmes &amp; Operations</td><td>Curator &amp; Researcher</td></tr>
+        <tr><td>Katherine Brown</td><td>Programmes &amp; Operations</td><td>Artist</td></tr>
+        <tr><td>Rob Birch</td><td>Marketing &amp; Comms</td><td>Artist</td></tr>
+        <tr><td>Beth Hart</td><td>Marketing &amp; Comms</td><td>Artist</td></tr>
+        <tr><td>Janet Robinson</td><td>Programmes &amp; Operations</td><td>Artist</td></tr>
+        <tr><td>Jackie Kennedy</td><td>Finance &amp; Fundraising</td><td>Artist</td></tr>
+        <tr><td>Stephen Foy-Philp</td><td>Programmes &amp; Operations</td><td>Artist</td></tr>
+        <tr><td>David Alderman</td><td>Finance &amp; Fundraising</td><td>Artist</td></tr>
+        <tr><td>Amber Baker</td><td>Programmes &amp; Operations</td><td>Artist</td></tr>
+      </tbody>
+    </table>
+  </div>
+</section>
+
+<?php snippet('site-footer') ?>
+
+<?php snippet('footer') ?>
