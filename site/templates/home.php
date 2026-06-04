ae1d9debd285ebf90e3bb94fdc99f59b1dd02cac
Francesco Imola <33965523+francescoimola@users.noreply.github.com>
2026-06-05 00:19:33 +0100

    Update home hero image + install kirby mcp
    Remove sponsor-related functionality and adjust home template. Deleted the sponsors structure from site/blueprints/pages/home.yml and removed site/blueprints/pages/sponsor-us.yml; dropped the sponsorLogo() method from site/models/home.php; removed the sponsors section/markup from site/templates/home.php. Also updated the hero background image src and object-position and tightened the intro paragraph formatting.

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,16 +1,14 @@
 <?php snippet('header') ?>
 
 <section class="hero stack-section flex text-center stack-gap-none">
-  <img class="hero__bg" src="/assets/images/specsavers.jpg" alt="" aria-hidden="true">
+  <img class="hero__bg" src="/assets/images/dover-road-studios.jpg" alt="" aria-hidden="true" style="object-position: 50% 25%">
   <h1>Open&nbsp;&nbsp;Art&nbsp;&nbsp;Folke</h1>
   <a class="hero__arrow fs-xl" href="#intro" aria-label="Scroll to content">↓</a>
 </section>
 
 <section id="intro" class="panel stack-section half layout-split theme-brand">
   <div class="fc readable">
-    <p class="statement">Open Art Folke is
-      <a href="#">a community of 200+ artists and makers</a>
-      in Folkestone. Since 2024, we've run
+    <p class="statement">Open Art Folke is a community of 200+ artists and makers in Folkestone. Since 2024, we've run
       <a href="#">an open house-style festival</a>
       to keep the creative energy alive, welcome people into our studios, and take over public spaces to share what we've been making.</p>
   </div>
